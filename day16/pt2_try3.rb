14f7d93b3b5e92f3d6f359a7dabe54109777fd40
Zack Young <zack.young@spiff.com>
2022-12-16 14:03:51 -0700

    Add actually optimized and simpler solution for Day 16

--- day16/pt2_try3.rb
+++ day16/pt2_try3.rb
@@ -20,11 +20,11 @@ File.readlines('input').each { |line|
   name, rate = row
   rate = rate.to_i
   id = id(name)
-  openable_valves << id if rate > 0 || name == "AA"
+  openable_valves << id if rate > 0
   flow_rates[id] = rate
   graph_node = graph[id]
   graph_node[id] = 0
-  links = row[2..].each { |link|
+  row[2..].each { |link|
     link_id = id(link)
     graph_node[link_id] = 1
   }
@@ -41,46 +41,44 @@ File.readlines('input').each { |line|
   }
 }
 
-start_valve_index = openable_valves.index(id("AA"))
+start_node = graph[id("AA")]
 max_time = 26
 best_scores = Hash.new { |h, k| h[k] = -1 }
 
 # Valve index, pressure so far, time left, valves opened (bit-string)
-state_queue = [[start_valve_index, 0, max_time, 0]]
+state_queue = openable_valves.each_with_index.map { |node_id, valve_index|
+  initial_distance = start_node[node_id] + 1
+  time_left = max_time - initial_distance
+  initial_pressure = flow_rates[node_id] * time_left
+  valve_bit_pos = 1 << valve_index
+  best_scores[[valve_index, valve_bit_pos]] = initial_pressure
+  [valve_index, initial_pressure, time_left, valve_bit_pos]
+}
 
 until state_queue.empty?
-  valve_index, released_pressure, time_left, valves_opened = state_queue.shift
-  bit_pos = 1 << valve_index
-  node_index = openable_valves[valve_index]
-  # Haven't opened this valve and still have time to open it
-  if (valves_opened & bit_pos) == 0 && time_left >= 1
-    new_valves_opened = valves_opened | bit_pos # Mark opened
-    new_time_left = time_left - 1
-    new_released_pressure = released_pressure + flow_rates[node_index] * new_time_left
-    score_key = [valve_index, new_valves_opened, new_time_left]
-    if best_scores[score_key] < new_released_pressure
-      best_scores[score_key] = new_released_pressure
-      state_queue << [valve_index, new_released_pressure, new_time_left, new_valves_opened]
-    end
-  end
+  valve_index, released_pressure, time_left, valves_opened = state_queue.pop
+  graph_node = graph[openable_valves[valve_index]]
+  openable_valves.each_with_index { |next_valve_id, next_valve_index|
+    next_valve_bit_pos = 1 << next_valve_index
+    next unless valves_opened & next_valve_bit_pos == 0 # Already been to this valve
 
-  graph_node = graph[node_index]
-  openable_valves.each_with_index { |next_valve, next_valve_index|
-    distance = graph_node[next_valve]
-    if distance <= time_left
-      new_time_left = time_left - distance
-      score_key = [next_valve_index, valves_opened, new_time_left]
-      if best_scores[score_key] < released_pressure
-        best_scores[score_key] = released_pressure
-        state_queue << [next_valve_index, released_pressure, new_time_left, valves_opened]
-      end
-    end
+    distance = graph_node[next_valve_id] + 1 # Get there and open it
+    next unless distance <= time_left
+
+    new_time_left = time_left - distance
+    new_released_pressure = released_pressure + flow_rates[next_valve_id] * new_time_left
+    new_valves_opened = valves_opened | next_valve_bit_pos
+    score_key = [next_valve_index, new_valves_opened]
+    next unless best_scores[score_key] < new_released_pressure
+
+    best_scores[score_key] = new_released_pressure
+    state_queue << [next_valve_index, new_released_pressure, new_time_left, new_valves_opened]
   }
 end
 
 permutation_count = 1 << openable_valves.size
 permutations = Array.new(permutation_count, 0)
-best_scores.each { |(_, valves_opened, _), released_pressure|
+best_scores.each { |(_, valves_opened), released_pressure|
   permutations[valves_opened] = released_pressure if released_pressure > permutations[valves_opened]
 }
 
